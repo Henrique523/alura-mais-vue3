504d21f5ab7b6b527ebaeddf790f33d4ac27e430
Guilherme Henrique Lemes de Godoy <guilherme.hlg.eng@gmail.com>
2022-11-09 14:37:10 -0300

    first commit

---
+++ src/components/OlaMundo.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Olá Mundo</h1>
+  <h2>Primeiro Projeto em VUE 3!!!</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "OlaMundo",
+});
+</script>
